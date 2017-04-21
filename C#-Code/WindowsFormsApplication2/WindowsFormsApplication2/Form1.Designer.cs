bf293f5b9808bedb9a800f124b7bbc402f7acfa4
Steve Aura <vysilhq.network@gmail.com>
2017-04-21 11:03:48 -0500

    Add files via upload

---
+++ C#-Code/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel_resources = new System.Windows.Forms.Panel();
+            this.panelS2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel96 = new System.Windows.Forms.Panel();
+            this.pictureBox42 = new System.Windows.Forms.PictureBox();
+            this.label81 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBarDashboardMain = new System.Windows.Forms.ProgressBar();
+            this.panel_resources.SuspendLayout();
+            this.panelS2.SuspendLayout();
+            this.panel96.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox42)).BeginInit();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(578, 372);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(333, 28);
+            this.comboBox4.TabIndex = 53;
+            this.comboBox4.Text = "Select Production Capacity";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(717, 320);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(333, 28);
+            this.comboBox5.TabIndex = 52;
+            this.comboBox5.Text = "Pwang, Nepal";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(707, 286);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(333, 28);
+            this.comboBox6.TabIndex = 51;
+            this.comboBox6.Text = "Potato";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel_resources
+            // 
+            this.panel_resources.BackColor = System.Drawing.Color.Transparent;
+            this.panel_resources.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel_resources.BackgroundImage")));
+            this.panel_resources.Controls.Add(this.panelS2);
+            this.panel_resources.Controls.Add(this.panel96);
+            this.panel_resources.Location = new System.Drawing.Point(0, 77);
+            this.panel_resources.Name = "panel_resources";
+            this.panel_resources.Size = new System.Drawing.Size(487, 365);
+            this.panel_resources.TabIndex = 71;
+            // 
+            // panelS2
+            // 
+            this.panelS2.BackColor = System.Drawing.Color.Transparent;
+            this.panelS2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelS2.BackgroundImage")));
+            this.panelS2.Controls.Add(this.label5);
+            this.panelS2.Controls.Add(this.label4);
+            this.panelS2.Controls.Add(this.label3);
+            this.panelS2.Controls.Add(this.Button2);
+            this.panelS2.Controls.Add(this.comboBox1);
+            this.panelS2.Controls.Add(this.comboBox3);
+            this.panelS2.Controls.Add(this.comboBox2);
+            this.panelS2.Location = new System.Drawing.Point(0, 44);
+            this.panelS2.Name = "panelS2";
+            this.panelS2.Size = new System.Drawing.Size(378, 452);
+            this.panelS2.TabIndex = 61;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(7, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 18);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "3rd Variable";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(10, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 18);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "2nd Variable";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(10, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "First Variable";
+            // 
+            // Button2
+            // 
+            this.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button2.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.Button2.Location = new System.Drawing.Point(0, 192);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(378, 32);
+            this.Button2.TabIndex = 50;
+            this.Button2.Text = "Project";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DATA..SOMETHING"});
+            this.comboBox1.Location = new System.Drawing.Point(10, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(331, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "DATA..SOMETHING";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "127820",
+            "184460",
+            "135093",
+            "97634",
+            "3003",
+            "11000"});
+            this.comboBox3.Location = new System.Drawing.Point(10, 93);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(331, 28);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.Text = "127820";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "10000",
+            "16000",
+            "15195",
+            "1291340"});
+            this.comboBox2.Location = new System.Drawing.Point(10, 158);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(331, 28);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = "15800";
+            // 
+            // panel96
+            // 
+            this.panel96.BackColor = System.Drawing.Color.Black;
+            this.panel96.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel96.BackgroundImage")));
+            this.panel96.Controls.Add(this.pictureBox42);
+            this.panel96.Controls.Add(this.label81);
+            this.panel96.Location = new System.Drawing.Point(0, 0);
+            this.panel96.Name = "panel96";
+            this.panel96.Size = new System.Drawing.Size(378, 47);
+            this.panel96.TabIndex = 0;
+            // 
+            // pictureBox42
+            // 
+            this.pictureBox42.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox42.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox42.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox42.Image")));
+            this.pictureBox42.Location = new System.Drawing.Point(10, 9);
+            this.pictureBox42.Name = "pictureBox42";
+            this.pictureBox42.Size = new System.Drawing.Size(31, 26);
+            this.pictureBox42.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox42.TabIndex = 29;
+            this.pictureBox42.TabStop = false;
+            // 
+            // label81
+            // 
+            this.label81.AutoSize = true;
+            this.label81.BackColor = System.Drawing.Color.Transparent;
+            this.label81.Font = new System.Drawing.Font("Franklin Gothic Book", 15.75F);
+            this.label81.ForeColor = System.Drawing.Color.White;
+            this.label81.Location = new System.Drawing.Point(45, 8);
+            this.label81.Name = "label81";
+            this.label81.Size = new System.Drawing.Size(302, 26);
+            this.label81.TabIndex = 28;
+            this.label81.Text = "Microsoft Azures (C# Pull - Basic)";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Transparent;
+            this.panel7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel7.BackgroundImage")));
+            this.panel7.Controls.Add(this.pictureBox1);
+            this.panel7.Controls.Add(this.progressBarDashboardMain);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(378, 77);
+            this.panel7.TabIndex = 51;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication2.Properties.Resources.Logbo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 55;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBarDashboardMain
+            // 
+            this.progressBarDashboardMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBarDashboardMain.Location = new System.Drawing.Point(0, 0);
+            this.progressBarDashboardMain.Name = "progressBarDashboardMain";
+            this.progressBarDashboardMain.Size = new System.Drawing.Size(378, 10);
+            this.progressBarDashboardMain.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(378, 342);
+            this.Controls.Add(this.panel_resources);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Microsoft Azures C# Pull (Basic)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_resources.ResumeLayout(false);
+            this.panelS2.ResumeLayout(false);
+            this.panelS2.PerformLayout();
+            this.panel96.ResumeLayout(false);
+            this.panel96.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox42)).EndInit();
+            this.panel7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel_resources;
+        private System.Windows.Forms.Panel panelS2;
+        internal System.Windows.Forms.ComboBox comboBox4;
+        internal System.Windows.Forms.ComboBox comboBox5;
+        internal System.Windows.Forms.ComboBox comboBox6;
+        internal System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Panel panel96;
+        private System.Windows.Forms.PictureBox pictureBox42;
+        private System.Windows.Forms.Label label81;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.ProgressBar progressBarDashboardMain;
+        private System.Windows.Forms.Timer timer1;
+        internal System.Windows.Forms.Label label5;
+        internal System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ C#-Code/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+            progressBarDashboardMain.Value = (0);
+            timer1.Start();
+           
+
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //Insert your Postman Client --> https://ussouthcentral.services.azureml.net/workspaces/ <-- your unique line here
+            var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/");
+            var request = new RestRequest(Method.POST);
+           
+            //Insert Postman-token
+            request.AddHeader("postman-token", "");
+            request.AddHeader("cache-control", "no-cache");
+           
+            //Insert, "Bearer followed by your Microsoft Azures API Key 
+            //For Example --> Bearer 35hsds359XZSX9sdsda3525== etc.
+            request.AddHeader("authorization", "Bearer ");
+            request.AddHeader("content-type", "application/json");
+           
+            //Below are the variables, make sure to insert your own 
+            //For Example --> 'Variable': \"" + comboBox1.Text + "\",
+            request.AddParameter("application/json", " INSERT YOUR POSTMAN BODY INTO THIS SECTION", ParameterType.RequestBody);
+
+            //Data Pulled
+            IRestResponse response = client.Execute(request);
+            label1.Text = response.Content.ToString();
+            progressBarDashboardMain.Increment(+1);
+
+
+            if (progressBarDashboardMain.Value == 100)
+            {
+                
+                timer1.Stop();
+                //These are the results that show
+                MessageBox.Show(label1.Text.ToString());
+            }
+        }
+    }
+}
+//CoderRevolt :D 
